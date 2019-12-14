774c7be88e26e506d930c24483e15cfdb661bd0c
minamar <mina.marmpena@gmail.com>
2019-12-14 18:21:04 +0100

    clean and corrected prior

--- beta_models_brms.R
+++ beta_models_brms.R
@@ -4,66 +4,35 @@ rm(list = ls(all.names = TRUE))
 library(brms)
 library(sjPlot)
 library(sjstats)
-library(ggplot2); theme_set(theme_bw())
+library(ggplot2); 
 library(loo)
-
-form1 = valence ~  v_cat * a_cat + trial + age + anim_experience + sex + (1|idAnim) + (1 |subject)
-form2 = valence ~  (1|subject)
-form3 = valence ~  (1|idAnim)
-form4 = valence ~ v_cat * a_cat
-form5 = valence ~  v_cat * a_cat + age + anim_experience + sex + (1|idAnim) + (1 |subject)
-form51 = valence ~  1 + v_cat * a_cat + age + anim_experience + sex + (1|idAnim) + (1 |subject)
-form52 = valence ~  v_cat + a_cat + age + anim_experience + sex + (1|idAnim) + (1 |subject)
-form53 = valence ~  0 + v_cat * a_cat + age + anim_experience + sex + (1|idAnim) + (1 |subject)
-form6 = valence ~ v_cat * a_cat + (1|idAnim) + (1 |subject)
-form7 = valence ~  v_cat * a_cat + age + anim_experience + sex + (1|idAnim) + (0 + v_cat |subject)
-form8 = valence ~  v_cat * a_cat + age + anim_experience + sex + (1|idAnim) + (1 + v_cat |subject)
-#form_ = valence ~ 1 + v_cat * a_cat + trial + age + anim_experience + sex + (1|idAnim) + (1 + v_cat|subject) # no diff
-form9 = arousal ~  v_cat * a_cat + age + anim_experience + sex + (1|idAnim) + (1 |subject)
-form54 = valence ~  v_cat * a_cat + age + anim_experience + sex + (1|idAnim) + (1 |subject) # probit link
-form10 = mvbind(valence, arousal) ~  v_cat * a_cat + age + anim_experience + sex + (1|p|idAnim) + (1|q|subject)
-form11 = mvbind(valence, arousal) ~  a_cat * v_cat + age + anim_experience + sex + (1|p|idAnim) + (1|q|subject)
-form12 = mvbind(valence, arousal) ~  v_cat * a_cat + age + anim_experience + sex 
-form13 = mvbind(valence, arousal) ~  v_cat * a_cat + age + anim_experience + sex + (1|idAnim) + (1|subject)
-form14 = mvbind(valence, arousal) ~  v_cat * a_cat + age + anim_experience + sex + (1+v_cat+a_cat|idAnim) + (1+v_cat+a_cat|subject)
-form15 = mvbind(valence, arousal) ~  v_cat * a_cat + age + anim_experience + sex + (1+v_cat+a_cat|idAnim) + (1|subject)
-form16 = mvbind(valence, arousal) ~  v_cat * a_cat + age + sex +  (1|p|idAnim) + (1|q|subject)
-form17 = mvbind(valence, arousal) ~  v_cat * a_cat + sex +  (1|idAnim) + (1|subject)
-form18 = bf(
-  mvbind(valence, arousal) ~  v_cat * a_cat + age + sex +  (1|idAnim) + (1|subject),
-  zoi ~ v_cat + a_cat
-)
-form19 = mvbind(valence, arousal) ~  v_cat * a_cat + sex + age + (1|idAnim) + (1|subject) # with informative priors
+library(dplyr)
+library(tibble)
+library(knitr)
+library(bayesplot)
+library(tidyverse)
+theme_set(bayesplot::theme_default(base_size = 14))
 
 def_prior = get_prior(form19, data = ratings, family = zero_one_inflated_beta(link = "probit", link_phi = "log", link_zoi = "logit", link_coi = "logit"))
 my_prior = c(
   #prior(lkj(1), class = cor),
              prior(beta(1,1), class = coi, resp = "arousal"),
              prior(normal(0.1767,0.10), class = Intercept, resp = "arousal"),
-             prior(normal(0.5398,0.10), class = b, coef=a_catr4, resp = "arousal"),
-             prior(normal(0.8373,0.10), class = b, coef=a_catr5, resp = "arousal"),
+             prior(normal(0.3631,0.10), class = b, coef=a_catr4, resp = "arousal"),
+             prior(normal(0.6606,0.10), class = b, coef=a_catr5, resp = "arousal"),
              prior(gamma(0.01, 0.01), class = phi, resp = "arousal"),
              prior(student_t(3, 0, 10), class = sd, resp = "arousal"),
              prior(beta(1, 1), class = zoi, resp = "arousal"),
              prior(beta(1,1), class = coi, resp = "valence"),
              prior(normal(0.1746,0.10), class = Intercept, resp = "valence"),
-             prior(normal(0.5058,0.10), class = b, coef=v_catNeu, resp = "valence"),
-             prior(normal(0.8154,0.10), class = b, coef=v_catPos, resp = "valence"),
+             prior(normal(0.3312,0.10), class = b, coef=v_catNeu, resp = "valence"),
+             prior(normal(0.6408,0.10), class = b, coef=v_catPos, resp = "valence"),
              prior(gamma(0.01, 0.01), class = phi, resp = "valence"),
              prior(student_t(3, 0, 10), class = sd, resp = "valence"),
              prior(beta(1, 1), class = zoi, resp = "valence")
              )
 
-my_prior_18 = c(
-  prior(normal(0.5,0.15), class = Intercept),                                               
-  prior(student_t(3, 0, 10), class = sd),
-  prior(logistic(0, 1), class = Intercept, dpar = coi),            
-  prior(student_t(3, 0, 10), class = sd, dpar = coi),            
-#  prior(student_t(3, 0, 10), class = Intercept, dpar = phi),            
-#  prior(student_t(3, 0, 10), class = sd, dpar =phi),            
-  prior(logistic(0, 1), class = Intercept, dpar =zoi),           
-  prior(student_t(3, 0, 10), class = sd, dpar = zoi)           
-)
+
   
 b_brms_m19 = brm(form19,
                data = ratings, 
@@ -75,7 +44,7 @@ b_brms_m19 = brm(form19,
                control = list(max_treedepth = 15, adapt_delta = 0.9999),
                autocor = NULL,
                save_all_pars = TRUE,
-               #sample_prior = "only",
+               sample_prior = TRUE,
                file = '/home/mina/Dropbox/APRIL-MINA/EXP4_EBL_GEN_VAE_USER/r_code/stan_models/b_brms_m19')
 
 mod = b_brms_m19
@@ -83,7 +52,7 @@ mod = b_brms_m19
 # Diagnostics
 summary(mod)
 plot(mod)
-pp = pp_check(mod, resp = "arousal")
+pp = pp_check(mod, resp = "valence")
 pp + theme_bw(mod)
 marginal_effects(mod)
 
@@ -94,11 +63,11 @@ b_brms_m6 <- add_criterion(b_brms_m6, "waic")
 loo_compare(b_brms_m6, b_brms_m1, criterion = "waic")
 
 ## Model comparison with loo
-b_brms_m17_loo = loo(b_brms_m17, save_psis = TRUE)
+b_brms_m18_loo = loo(b_brms_m18, save_psis = TRUE)
 
 # If there is warning pareto_k > 0.7 
-b_brms_m17_reloo = loo(b_brms_m17, reloo = T)
-loo_compare( b_brms_m17_reloo, b_brms_m16_reloo, b_brms_m10p_reloo,b_brms_m10_reloo, b_brms_m0_loo, b_brms_m1_reloo, b_brms_m2_loo, b_brms_m3_loo, b_brms_m4_loo, b_brms_m5_loo, b_brms_m6_reloo, b_brms_m7_reloo, b_brms_m8_loo, b_brms_m52_reloo)
+b_brms_m18_reloo = loo(b_brms_m18, reloo = T)
+loo_compare(b_brms_m20_loo,b_brms_m18_reloo, b_brms_m19_reloo, b_brms_m0_loo, b_brms_m1_reloo, b_brms_m2_loo, b_brms_m3_loo, b_brms_m4_loo, b_brms_m5_loo, b_brms_m6_reloo, b_brms_m7_reloo, b_brms_m8_loo, b_brms_m52_reloo)
 
 
 # 
@@ -126,16 +95,33 @@ df_plot = cbind(newdata, fit)
 # Transform with posterior estimates with antilogit and prepare coefs for contrasts
 coda = brms::posterior_samples(mod)
 antilogit = function(x) 1 / (1 + exp(-x))   # same as plogis()
-fit2 = data.frame(v_catNeg =      antilogit(coda$b_Intercept),
-                  a_catr3 =      antilogit(coda$b_Intercept),
-                    v_catNeu =    antilogit(coda$b_Intercept + coda$b_v_catNeu),
-                    v_catPos =   antilogit(coda$b_Intercept + coda$b_v_catPos),
-                    a_catr4 = antilogit(coda$b_Intercept + coda$b_a_catr4),
-                    a_catr5 =    antilogit(coda$b_Intercept + coda$b_a_catr5),
-                    #trial = antilogit(coda$b_Intercept + coda$b_trial),
-                    age =   antilogit(coda$b_Intercept + coda$b_age), 
-                    anim_experience =    antilogit(coda$b_Intercept + coda$b_anim_experience),
-                    sexM =   antilogit(coda$b_Intercept + coda$b_sexM)
+
+fit1 = data.frame(v_catNeg_zoi = antilogit(coda$b_zoi_valence_Intercept),
+                 a_catr3_zoi =      antilogit(coda$b_zoi_arousal_Intercept),
+                 v_catNeu_zoi =    antilogit(coda$b_zoi_valence_Intercept + coda$b_zoi_valence_v_catNeu),
+                 v_catPos_zoi =   antilogit(coda$b_zoi_valence_Intercept + coda$b_zoi_valence_v_catPos),
+                 a_catr4_zoi = antilogit(coda$b_zoi_arousal_Intercept + coda$b_zoi_arousal_a_catr4),
+                 a_catr5_zoi =    antilogit(coda$b_zoi_arousal_Intercept + coda$b_zoi_arousal_a_catr5)
+)
+
+posterior_samples(mod, pars = "b_")[,13:24] %>% 
+  #mutate_at(c("b_zoi_valence_Intercept"), plogis) %>% 
+  mutate_all( plogis) %>% 
+  posterior_summary() %>% 
+  as.data.frame() %>% 
+  rownames_to_column("Parameter") %>% 
+  kable(digits = 2) 
+
+fit2 = data.frame(v_catNeg =      antilogit(coda$b_valence_Intercept),
+                  a_catr3 =      antilogit(coda$b_arousal_Intercept),
+                    v_catNeu =    antilogit(coda$b_valence_Intercept + coda$b_valence_v_catNeu),
+                    v_catPos =   antilogit(coda$b_valence_Intercept + coda$b_valence_v_catPos),
+                    a_catr4 = antilogit(coda$b_arousal_Intercept + coda$b_valence_a_catr4),
+                    a_catr5 =    antilogit(coda$b_arousal_Intercept + coda$b_valence_a_catr5),
+                    v_age =   antilogit(coda$b_valence_Intercept + coda$b_valence_age), 
+                    a_age =   antilogit(coda$b_arousal_Intercept + coda$b_arousal_age), 
+                    v_sexM =   antilogit(coda$b_valence_Intercept + coda$b_valence_sexM), 
+                    a_sexM = antilogit(coda$b_arousal_Intercept + coda$b_arousal_sexM)
                     # v_catNeg_a_catr3 = antilogit(coda$b_Intercept),
                     # v_catNeu_a_catr3 = antilogit(coda$b_Intercept + coda$b_v_catNeu),
                     # v_catPos_a_catr3 = antilogit(coda$b_Intercept + coda$b_v_catPos),
@@ -148,7 +134,7 @@ fit2 = data.frame(v_catNeg =      antilogit(coda$b_Intercept),
 )
 
 # Contrasts analysis
-neu_vs_neg = fit2$v_catNeu - fit2$v_catNeg
+neu_vs_neg = fit1$v_catNeu_zoi - fit1$v_catNeg_zoi
 hist(neu_vs_neg)
 quantile(neu_vs_neg, probs = c(.5, .025, .975)) 
 mean(neu_vs_neg > 0)
@@ -192,13 +178,13 @@ dimnames(posterior)
 mcmc_intervals(posterior, pars = vars(starts_with("r_subject")))
 mcmc_intervals(posterior, pars = vars(starts_with("r_idAnim")))
 
-# Prior plots
+# Prior predictive plots
 conditions <- data.frame(v_cat = unique(ratings$v_cat))
 rownames(conditions) <- unique(ratings$v_cat)
 me_loss_prior1 <- marginal_effects(
-  b_brms_m5, conditions = conditions, 
-  re_formula = NULL, method = "predict"
+  mod, conditions = conditions, 
+  re_formula = NULL, method = "fitted"
 )
-p0 <- plot(me_loss_prior1, ncol = 3, points = TRUE, plot = FALSE)
+plot(me_loss_prior1, ncol = 3, points = TRUE, plot = TRUE)
 p0$dev + ggtitle("Prior predictive distributions")
 
