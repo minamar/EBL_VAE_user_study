47130abe9c6206b805744922b079d8d89246cbcc
minamar <mina.marmpena@gmail.com>
2020-02-23 20:45:17 +0100

    plots

--- designed_vs_generated.R
+++ designed_vs_generated.R
@@ -35,15 +35,15 @@ names(m_anth) <- c("subject", "sex", "condition", "likert")
 # Anthropomorphism- All groups
 # Replace condition with names of groups
 anth_all <- m_anth %>%
-  mutate(condition = replace(condition, condition == 28, "des_pre")) %>% 
-  mutate(condition = replace(condition, condition == 29, "gen_pre")) %>%
-  mutate(condition = replace(condition, condition == 30, "des_post")) %>%
-  mutate(condition = replace(condition, condition == 31, "gen_post"))
+  mutate(condition = replace(condition, condition == 28, "designed_pre")) %>% 
+  mutate(condition = replace(condition, condition == 29, "generated_pre")) %>%
+  mutate(condition = replace(condition, condition == 30, "designed_post")) %>%
+  mutate(condition = replace(condition, condition == 31, "generated_post"))
 
 anth_all$likert = ordered(anth_all$likert)
 anth_all$likert = as.numeric(anth_all$likert)
 anth_all$likert = factor(anth_all$likert, levels = 1:5)
-anth_all$condition = factor(anth_all$condition, levels = c("des_pre", "gen_pre", "des_post", "gen_post"))
+anth_all$condition = factor(anth_all$condition, levels = c("designed_pre", "generated_pre", "designed_post", "generated_post"))
 # anth_all_count = anth_all[,2:3] %>%
 #   group_by(condition, likert) %>%
 #   summarize(Count = n())
@@ -59,9 +59,8 @@ p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
 (ctable <- cbind(ctable, "p value" = p))
 
 # Create predictions
-df_both = data.frame(condition = rep(c("des_pre", "gen_pre", "des_post", "gen_post"), each = 5),  
-                     likert = rep(c(1:5), 2),
-                     sex = rep(c("M", "F"), each = 5))
+df_both = data.frame(condition = rep(c("designed_pre", "generated_pre", "designed_post", "generated_post"), each = 5),  
+                     likert = rep(c(1:5), 2))
 
 polr_both_probs = cbind(df_both, predict(model, newdata = df_both, type = "probs", se = TRUE))
 polr_both_probs = polr_both_probs[,-2]
@@ -72,6 +71,7 @@ polr_both_probs_df = reshape2::melt(polr_both_probs, id.vars = "condition",
 # Figure 
 g_anth_all <- ggplot(polr_both_probs_df, aes(x = likert, y = probability, 
                                color = condition, group = condition)) +
+  scale_color_discrete(breaks=c("designed_pre", "generated_pre", "designed_post", "generated_post")) +
   geom_line() + 
   geom_point() +
   xlab("Anthropomorphism")
@@ -81,13 +81,13 @@ g_anth_all <- ggplot(polr_both_probs_df, aes(x = likert, y = probability,
 # Anthropomorphism- pre-post groups
 # Replace condition with names of groups
 anth_pre_post <- m_anth %>%
-  mutate(condition = replace(condition, condition == 28 | condition == 29, "pre")) %>% 
-  mutate(condition = replace(condition, condition == 30 | condition == 31, "post")) 
+  mutate(condition = replace(condition, condition == 28 | condition == 29, "pretest")) %>% 
+  mutate(condition = replace(condition, condition == 30 | condition == 31, "posttest")) 
 
 anth_pre_post$likert = ordered(anth_pre_post$likert)
 anth_pre_post$likert = as.numeric(anth_pre_post$likert)
 anth_pre_post$likert = factor(anth_pre_post$likert, levels = 1:5)
-anth_pre_post$condition = factor(anth_pre_post$condition, levels = c("pre", "post"))
+anth_pre_post$condition = factor(anth_pre_post$condition, levels = c("pretest", "posttest"))
 
 # anth_all_count = anth_all[,2:3] %>%
 #   group_by(condition, likert) %>%
@@ -104,7 +104,7 @@ p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
 (ctable <- cbind(ctable, "p value" = p))
 
 # Create predictions
-df_both = data.frame(condition = rep(c("pre", "post"), each = 5),  
+df_both = data.frame(condition = rep(c("pretest", "posttest"), each = 5),  
                      likert = rep(c(1:5), 2))
 polr_both_probs = cbind(df_both, predict(model, newdata = df_both, type = "probs", se = TRUE))
 polr_both_probs = polr_both_probs[,-2]
@@ -115,6 +115,7 @@ polr_both_probs_df = reshape2::melt(polr_both_probs, id.vars = "condition",
 # Figure 
 g_anth_pre_post <- ggplot(polr_both_probs_df, aes(x = likert, y = probability, 
                                color = condition, group = condition)) +
+  scale_color_discrete(breaks=c("pretest", "posttest")) +
   geom_line() + 
   geom_point() +
   xlab("Anthropomorphism")
@@ -153,6 +154,7 @@ polr_both_probs_df = reshape2::melt(polr_both_probs, id.vars = "sex",
 # Figure 
 g_anth_sex <- ggplot(polr_both_probs_df, aes(x = likert, y = probability, 
                                color = sex, group = sex)) +
+  scale_color_manual(name="Gender", labels = c("Female", "Male"), values=c("#7CAE00","#C77CFF")) + 
   geom_line() + 
   geom_point() +
   xlab("Anthropomorphism")
@@ -170,15 +172,15 @@ names(m_anim) <- c("subject", "sex", "condition", "likert")
 # Animacy- All groups
 # Replace condition with names of groups
 anim_all <- m_anim %>%
-  mutate(condition = replace(condition, condition == 28, "des_pre")) %>% 
-  mutate(condition = replace(condition, condition == 29, "gen_pre")) %>%
-  mutate(condition = replace(condition, condition == 30, "des_post")) %>%
-  mutate(condition = replace(condition, condition == 31, "gen_post"))
+  mutate(condition = replace(condition, condition == 28, "designed_pre")) %>% 
+  mutate(condition = replace(condition, condition == 29, "generated_pre")) %>%
+  mutate(condition = replace(condition, condition == 30, "designed_post")) %>%
+  mutate(condition = replace(condition, condition == 31, "generated_post"))
 
 anim_all$likert = ordered(anim_all$likert)
 anim_all$likert = as.numeric(anim_all$likert)
 anim_all$likert = factor(anim_all$likert, levels = 1:5)
-anim_all$condition = factor(anim_all$condition, levels = c("des_pre", "gen_pre", "des_post", "gen_post"))
+anim_all$condition = factor(anim_all$condition, levels = c("designed_pre", "generated_pre", "designed_post", "generated_post"))
 
 model = polr(likert ~ condition,  data = anim_all, Hess = TRUE)
 
@@ -193,7 +195,7 @@ p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
 
 
 # Create predictions
-df_both = data.frame(condition = rep(c("des_pre", "gen_pre", "des_post", "gen_post"), each = 5),  
+df_both = data.frame(condition = rep(c("designed_pre", "generated_pre", "designed_post", "generated_post"), each = 5),  
                      likert = rep(c(1:5), 2))
 polr_both_probs = cbind(df_both, predict(model, newdata = df_both, type = "probs", se = TRUE))
 polr_both_probs = polr_both_probs[,-2]
@@ -204,6 +206,8 @@ polr_both_probs_df = reshape2::melt(polr_both_probs, id.vars = "condition",
 # Figure 
 g_anim_all <- ggplot(polr_both_probs_df, aes(x = likert, y = probability, 
                                color = condition, group = condition)) +
+  scale_color_discrete(breaks=c("designed_pre", "generated_pre", "designed_post", "generated_post")) +
+  labs(color='Animation group')  +
   geom_line() + 
   geom_point() +
   xlab("Animacy")
@@ -212,13 +216,13 @@ g_anim_all <- ggplot(polr_both_probs_df, aes(x = likert, y = probability,
 # Animacy- pre-post groups
 # Replace condition with names of groups
 anim_pre_post <- m_anim %>%
-  mutate(condition = replace(condition, condition == 28 | condition == 29, "pre")) %>% 
-  mutate(condition = replace(condition, condition == 30 | condition == 31, "post")) 
+  mutate(condition = replace(condition, condition == 28 | condition == 29, "pretest")) %>% 
+  mutate(condition = replace(condition, condition == 30 | condition == 31, "posttest")) 
 
 anim_pre_post$likert = ordered(anim_pre_post$likert)
 anim_pre_post$likert = as.numeric(anim_pre_post$likert)
 anim_pre_post$likert = factor(anim_pre_post$likert, levels = 1:5)
-anim_pre_post$condition = factor(anim$condition, levels = c("pre", "post"))
+anim_pre_post$condition = factor(anim_pre_post$condition, levels = c("pretest", "posttest"))
 
 model = polr(likert ~ condition,  data = anim_pre_post, Hess = TRUE)
 summary(model)
@@ -231,7 +235,7 @@ p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
 (ctable <- cbind(ctable, "p value" = p))
 
 # Create predictions
-df_both = data.frame(condition = rep(c("pre", "post"), each = 5),  
+df_both = data.frame(condition = rep(c("pretest", "posttest"), each = 5),  
                      likert = rep(c(1:5), 2))
 polr_both_probs = cbind(df_both, predict(model, newdata = df_both, type = "probs", se = TRUE))
 polr_both_probs = polr_both_probs[,-2]
@@ -242,6 +246,8 @@ polr_both_probs_df = reshape2::melt(polr_both_probs, id.vars = "condition",
 # Figure 
 g_anim_pre_post <- ggplot(polr_both_probs_df, aes(x = likert, y = probability, 
                                color = condition, group = condition)) +
+  scale_color_discrete(breaks=c("pretest", "posttest")) +
+  labs(color='Animation group')  +
   geom_line() + 
   geom_point() +
   xlab("Animacy")
@@ -277,7 +283,49 @@ polr_both_probs_df = reshape2::melt(polr_both_probs, id.vars = "sex",
 # Figure 
 g_anim_sex <- ggplot(polr_both_probs_df, aes(x = likert, y = probability, 
                                color = sex, group = sex)) +
+  scale_color_manual(name="Gender", labels = c("Female", "Male"), values=c("#7CAE00","#C77CFF")) + 
   geom_line() + 
   geom_point() +
   xlab("Animacy")
 
+
+###########################################################
+# Plots
+library(cowplot)
+
+# Plot gen vs des 
+p_all <- plot_grid(g_anth_all + theme(legend.position = "none"), g_anim_all + theme(legend.position = "none"), labels = "AUTO", ncol = 2)
+legend <- get_legend(
+  g_anim_all +
+    guides(color = guide_legend(nrow = 1)) +
+    theme(legend.position = "bottom")
+)
+# add the legend to the row we made earlier. Give it one-third of 
+# the width of one plot (via rel_widths).
+plot_grid(p_all, legend,  ncol = 1, rel_heights = c(1, .1))
+ggsave("images/anth_anim_all.pdf", units="mm", width=200, height=80, dpi=300)
+
+# Plot pre vs post
+p_pre_post <- plot_grid(g_anth_pre_post + theme(legend.position = "none"), g_anim_pre_post + theme(legend.position = "none"), labels = "AUTO", ncol = 2)
+legend <- get_legend(
+  g_anim_pre_post +
+    guides(color = guide_legend(nrow = 1)) +
+    theme(legend.position = "bottom")
+)
+# add the legend to the row we made earlier. Give it one-third of 
+# the width of one plot (via rel_widths).
+plot_grid(p_pre_post, legend,  ncol = 1, rel_heights = c(1, .1))
+ggsave("images/anth_anim_pre_post.pdf", units="mm", width=200, height=80, dpi=300)
+
+# Plot female vs male
+
+p_sex <- plot_grid(g_anth_sex + theme(legend.position = "none"), g_anim_sex + theme(legend.position = "none"), labels = "AUTO", ncol = 2)
+legend <- get_legend(
+  g_anim_sex +
+    guides(color = guide_legend(nrow = 1)) +
+    theme(legend.position = "bottom")
+)
+# add the legend to the row we made earlier. Give it one-third of 
+# the width of one plot (via rel_widths).
+plot_grid(p_sex, legend,  ncol = 1, rel_heights = c(1, .1))
+ggsave("images/anth_anim_sex.pdf", units="mm", width=200, height=80, dpi=300)
