11e4660cde3f2c511a56075a230380ae6f3f213e
minamar <mina.marmpena@gmail.com>
2020-02-25 12:10:04 +0100

    ci lim, nominal test

--- designed_vs_generated.R
+++ designed_vs_generated.R
@@ -42,14 +42,15 @@ anth_all <- m_anth %>%
 
 anth_all$likert = ordered(anth_all$likert)
 anth_all$likert = as.numeric(anth_all$likert)
-anth_all$likert = factor(anth_all$likert, levels = 1:5)
-anth_all$condition = factor(anth_all$condition, levels = c("designed_pre", "generated_pre", "designed_post", "generated_post"))
-# anth_all_count = anth_all[,2:3] %>%
-#   group_by(condition, likert) %>%
-#   summarize(Count = n())
+anth_all$likert = factor(anth_all$likert, levels = 1:5, ordered = TRUE)
+anth_all$condition = factor(anth_all$condition, levels = c("designed_pre", "generated_pre", "designed_post", "generated_post"), ordered = FALSE)
 
 model = polr(likert ~ condition,  data = anth_all, Hess = TRUE)
 summary(model)
+
+# Uncomment only to get theh nominal test
+# model_clm = clm(likert ~ condition,  data = anth_all, link = "logit")
+# nominal_test(model_clm)
 
 marginal = lsmeans(model, ~condition)
 pairs(marginal, adjust="tukey")
@@ -57,6 +58,7 @@ pairs(marginal, adjust="tukey")
 ctable <- coef(summary(model))
 p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
 (ctable <- cbind(ctable, "p value" = p))
+(ci <- confint(model)) 
 
 # Create predictions
 df_both = data.frame(condition = rep(c("designed_pre", "generated_pre", "designed_post", "generated_post"), each = 5),  
@@ -67,6 +69,7 @@ polr_both_probs = polr_both_probs[,-2]
 polr_both_probs = unique(polr_both_probs)
 polr_both_probs_df = reshape2::melt(polr_both_probs, id.vars = "condition", 
                                     variable.name = "likert", value.name = "probability")
+
 
 # Figure 
 g_anth_all <- ggplot(polr_both_probs_df, aes(x = likert, y = probability, 
@@ -86,15 +89,15 @@ anth_pre_post <- m_anth %>%
 
 anth_pre_post$likert = ordered(anth_pre_post$likert)
 anth_pre_post$likert = as.numeric(anth_pre_post$likert)
-anth_pre_post$likert = factor(anth_pre_post$likert, levels = 1:5)
+anth_pre_post$likert = factor(anth_pre_post$likert, levels = 1:5, ordered = TRUE)
 anth_pre_post$condition = factor(anth_pre_post$condition, levels = c("pretest", "posttest"))
-
-# anth_all_count = anth_all[,2:3] %>%
-#   group_by(condition, likert) %>%
-#   summarize(Count = n())
 
 model = polr(likert ~ condition,  data = anth_pre_post, Hess = TRUE)
 summary(model)
+
+# Uncomment only to get theh nominal test
+# model_clm = clm(likert ~ condition,  data = anth_pre_post, link = "logit")
+# nominal_test(model_clm)
 
 marginal = lsmeans(model, ~condition)
 pairs(marginal, adjust="tukey")
@@ -102,6 +105,7 @@ pairs(marginal, adjust="tukey")
 ctable <- coef(summary(model))
 p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
 (ctable <- cbind(ctable, "p value" = p))
+(ci <- confint(model)) 
 
 # Create predictions
 df_both = data.frame(condition = rep(c("pretest", "posttest"), each = 5),  
@@ -126,14 +130,14 @@ anth_sex <- m_anth
 
 anth_sex$likert = ordered(anth_sex$likert)
 anth_sex$likert = as.numeric(anth_sex$likert)
-anth_sex$likert = factor(anth_sex$likert, levels = 1:5)
-
-# anth_all_count = anth_all[,2:3] %>%
-#   group_by(condition, likert) %>%
-#   summarize(Count = n())
+anth_sex$likert = factor(anth_sex$likert, levels = 1:5, ordered = TRUE)
 
 model = polr(likert ~ sex,  data = anth_sex, Hess = TRUE)
 summary(model)
+
+# Uncomment only to get theh nominal test
+# model_clm = clm(likert ~ sex,  data = anth_sex, link = "logit")
+# nominal_test(model_clm)
 
 marginal = lsmeans(model, ~sex)
 pairs(marginal, adjust="tukey")
@@ -141,6 +145,7 @@ pairs(marginal, adjust="tukey")
 ctable <- coef(summary(model))
 p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
 (ctable <- cbind(ctable, "p value" = p))
+(ci <- confint(model)) 
 
 # Create predictions
 df_both = data.frame(sex = rep(c("F", "M"), each = 5),  
@@ -179,12 +184,15 @@ anim_all <- m_anim %>%
 
 anim_all$likert = ordered(anim_all$likert)
 anim_all$likert = as.numeric(anim_all$likert)
-anim_all$likert = factor(anim_all$likert, levels = 1:5)
-anim_all$condition = factor(anim_all$condition, levels = c("designed_pre", "generated_pre", "designed_post", "generated_post"))
+anim_all$likert = factor(anim_all$likert, levels = 1:5, ordered = TRUE)
+anim_all$condition = factor(anim_all$condition, levels = c("designed_pre", "generated_pre", "designed_post", "generated_post"), ordered = FALSE)
 
 model = polr(likert ~ condition,  data = anim_all, Hess = TRUE)
-
 summary(model)
+
+# Uncomment only to get theh nominal test
+# model_clm = clm(likert ~ condition,  data = anim_all, link = "logit")
+# nominal_test(model_clm)
 
 marginal = lsmeans(model, ~condition)
 pairs(marginal, adjust="tukey")
@@ -192,7 +200,7 @@ pairs(marginal, adjust="tukey")
 ctable <- coef(summary(model))
 p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
 (ctable <- cbind(ctable, "p value" = p))
-
+(ci <- confint(model)) 
 
 # Create predictions
 df_both = data.frame(condition = rep(c("designed_pre", "generated_pre", "designed_post", "generated_post"), each = 5),  
@@ -221,11 +229,15 @@ anim_pre_post <- m_anim %>%
 
 anim_pre_post$likert = ordered(anim_pre_post$likert)
 anim_pre_post$likert = as.numeric(anim_pre_post$likert)
-anim_pre_post$likert = factor(anim_pre_post$likert, levels = 1:5)
+anim_pre_post$likert = factor(anim_pre_post$likert, levels = 1:5, ordered = TRUE)
 anim_pre_post$condition = factor(anim_pre_post$condition, levels = c("pretest", "posttest"))
 
 model = polr(likert ~ condition,  data = anim_pre_post, Hess = TRUE)
 summary(model)
+
+# Uncomment only to get theh nominal test
+# model_clm = clm(likert ~ condition,  data = anim_pre_post, link = "logit")
+# nominal_test(model_clm)
 
 marginal = lsmeans(model, ~condition)
 pairs(marginal, adjust="tukey")
@@ -233,6 +245,7 @@ pairs(marginal, adjust="tukey")
 ctable <- coef(summary(model))
 p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
 (ctable <- cbind(ctable, "p value" = p))
+(ci <- confint(model)) 
 
 # Create predictions
 df_both = data.frame(condition = rep(c("pretest", "posttest"), each = 5),  
@@ -258,11 +271,14 @@ anim_sex <- m_anim
 
 anim_sex$likert = ordered(anim_sex$likert)
 anim_sex$likert = as.numeric(anim_sex$likert)
-anim_sex$likert = factor(anim_sex$likert, levels = 1:5)
-
+anim_sex$likert = factor(anim_sex$likert, levels = 1:5, ordered = TRUE)
 
 model = polr(likert ~ sex,  data = anim_sex, Hess = TRUE)
 summary(model)
+
+# Uncomment only to get theh nominal test
+# model_clm = clm(likert ~ sex,  data = anim_sex, link = "logit")
+# nominal_test(model_clm)
 
 marginal = lsmeans(model, ~sex)
 pairs(marginal, adjust="tukey")
@@ -270,6 +286,7 @@ pairs(marginal, adjust="tukey")
 ctable <- coef(summary(model))
 p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
 (ctable <- cbind(ctable, "p value" = p))
+(ci <- confint(model)) 
 
 # Create predictions
 df_both = data.frame(sex = rep(c("F", "M"), each = 5),  
